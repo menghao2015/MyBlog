24cf1fb9ba03e88c45e283e5a48091fca8041413
root <root@localhost.localdomain>
2016-04-11 14:29:30 +0800

    4-11_1-17c

--- myapp/main/views.py
+++ myapp/main/views.py
@@ -15,6 +15,7 @@ def index():
 	if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
 		post = Post(body=form.body.data, author=current_user._get_current_object())
 		db.session.add(post)
+		db.session.commit()
 		return redirect(url_for('.index'))
 	page = request.args.get('page', 1, type=int)
 	show_followed = False
@@ -45,6 +46,7 @@ def post(id):
 		comment = Comment(body=form.body.data, post = post, 
 							author = current_user._get_current_object())
 		db.session.add(comment)
+		db.session.commit()
 		flash('your comment has been published')
 		return redirect(url_for('.post', id=post.id, page=-1))
 	page = request.args.get('page', 1, type=int)
@@ -67,6 +69,7 @@ def edit_post(id):
 			abort(403)
 		post.body = form.body.data
 		db.session.add(post)
+		db.session.commit()
 		flash('The posts has been update.')
 		return redirect(url_for('.post', id=id))
 	form.body.data = post.body
@@ -82,6 +85,7 @@ def edit_profile():
 		current_user.location = form.location.data
 		current_user.about_me = form.about_me.data
 		db.session.add(current_user)
+		db.session.commit()
 		flash(' Your profile has been updated ')
 		return redirect(url_for('.user', username=current_user.username))
 	form.name.data = current_user.name
@@ -105,6 +109,7 @@ def edit_profile_admin(id):
 		user.location = form.location.data
 		user.about_me = form.about_me.data
 		db.session.add(user)
+		db.session.commit()
 		flash('Your profile has been updated')
 		return redirect(url_for('.user', username = user.username))
 	form.email.data = user.email
@@ -205,6 +210,7 @@ def moderate_enable(id):
 	comment = Comment.query.get_or_404(id)
 	comment.disabled = False
 	db.session.add(comment)
+	db.session.commit()
 	return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
 
 
@@ -215,6 +221,7 @@ def moderate_disable(id):
 	comment = Comment.query.get_or_404(id)
 	comment.disabled = True
 	db.session.add(comment)
+	db.session.commit()
 	return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
 
 
