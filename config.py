4bddb42d153264441b2d88c57426f7afdb997266
root <root@localhost.localdomain>
2016-04-12 11:30:01 +0800

    4-12_1

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	SQLALCHEMY_POOL_RECYCLE = 10
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'Flasky Admin <lucky__menghao@163.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'menghao_92@163.com'
